d888db52209af605ee792292978a1b08e7f8e5ff
lqj1 <1104942848@qq.com>
2021-05-26 00:26:58 +0800

    add new

--- projects-demo/06-heima-news/toutiao-m/src/utils/request.js
+++ projects-demo/06-heima-news/toutiao-m/src/utils/request.js
@@ -2,6 +2,7 @@
  * 封装 axios 请求模块
  */
 import axios from 'axios'
+// 请求定义给 request，方便导出，以及后续的拦截器处理
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
 })
--- projects-demo/06-heima-news/toutiao-m/src/views/login/index.vue
+++ projects-demo/06-heima-news/toutiao-m/src/views/login/index.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="login-container">
-    <van-nav-bar
-      title="标题"
-      left-arrow
-    />
+    <!-- 导航栏 -->
+    <van-nav-bar class="app-nav-bar" title="注册 / 登录" left-arrow @click-left="$router.back()" />
+    <!-- 导航栏 -->
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field v-model="user.mobile" icon-prefix="icon" left-icon="shouji" placeholder="请输入手机号" />
+      <van-field v-model="user.code" icon-prefix="icon" left-icon="yanzhengma" placeholder="请输入验证码">
+        <template #button>
+          <van-button class="send-btn" size="small" round>发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <div class="login-btn-wrap">
+      <van-button class="login-btn" type="info" block @click="onLogin">登录</van-button>
+    </div>
+    <!-- 登录表单 -->
   </div>
 </template>
 
 <script>
+import { login } from '@/api/user'
 export default {
-  name: 'loginIndex'
+  name: 'loginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
+  },
+  methods: {
+    async onLogin () {
+      // 1. 找到数据接口
+      // 2. 封装请求方法
+      // 3. 请求调用登录
+      try {
+        const res = await login(this.user)
+        // 4. 处理相应结果
+        console.log(res)
+      } catch (err) {
+        console.log(err)
+        console.log('登录失败', err)
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.login-container {
+  .send-btn {
+    width: 78px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text {
+      font-size: 11px;
+      color: #666666
+    }
+  }
+  .login-btn-wrap {
+    padding: 26px 16px;
+    .login-btn {
+      background-color: #6db4fb;;
+      border: none;
+      .van-button__text {
+        font-size: 15px;
+      }
+    }
+  }
+}
 
 </style>
