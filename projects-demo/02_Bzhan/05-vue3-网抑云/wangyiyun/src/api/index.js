7f13b7d360fdbec50a808d395fab16db8a9122f5
lqj1 <1104942848@qq.com>
2021-09-05 22:11:15 +0800

    update at home

--- projects-demo/02_Bzhan/05-vue3-网抑云/wangyiyun/src/api/index.js
+++ projects-demo/02_Bzhan/05-vue3-网抑云/wangyiyun/src/api/index.js
@@ -1,5 +1,15 @@
-import axios from "axios";
+import { request } from "./request";
 // 获取轮播图api，type：资源类型，对应以下类型，默认0为pc，1为安卓，2为iphone，3位ipad
 export function getBanners(type = 1) {
-  return axios.get(`http://localhost:3000/banner?type=${type}`);
+  return request({
+    url: `/banner?type=${type}`,
+    data: type
+  })
+}
+// 获取推荐歌单，limit：取出数量，默认10条数据
+export function getMusicList (limit = 10) {
+  return request({
+    url: `/personalized?limit=${limit}`,
+    data: limit
+  })
 }
--- projects-demo/02_Bzhan/05-vue3-网抑云/wangyiyun/src/components/MusicList.vue
+++ projects-demo/02_Bzhan/05-vue3-网抑云/wangyiyun/src/components/MusicList.vue
@@ -7,16 +7,16 @@
     <div class="mlist">
       <div v-swiper:mySwiper="swiperOption" class="swiper-container">
         <div class="swiper-wrapper">
-          <div class="swiper-slide">Slide 1</div>
-          <div class="swiper-slide">Slide 2</div>
-          <div class="swiper-slide">Slide 3</div>
-          <div class="swiper-slide">Slide 4</div>
-          <div class="swiper-slide">Slide 5</div>
-          <div class="swiper-slide">Slide 6</div>
-          <div class="swiper-slide">Slide 7</div>
-          <div class="swiper-slide">Slide 8</div>
-          <div class="swiper-slide">Slide 9</div>
-          <div class="swiper-slide">Slide 10</div>
+          <div class="swiper-slide" v-for="(item,index) in musicList" :key="index">
+            <img :src="item.picUrl" alt="">
+            <div class="name">{{item.name}}</div>
+            <div class="count">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-xiangyousanjiaoxing"></use>
+              </svg>
+              {{formatValue(item.playCount)}}
+            </div>
+          </div>
         </div>
         <div class="swiper-pagination"></div>
       </div>
@@ -25,31 +25,54 @@
 </template>
 
 <script>
+import { getMusicList } from '@/api/index.js'
 import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
 // import style (>= Swiper 6.x)
 // import 'swiper/swiper-bundle.css'
 // import style (<= Swiper 5.x)
 import 'swiper/css/swiper.css'
 
-export default {
-  components: {
-  },
-  data () {
-    return {
-      swiperOption: {
-        slidesPerView: 3, // 一行显示多少张
-        spaceBetween: 20,
-        pagination: {
-          el: '.swiper-pagination'
-        },
-        // ...
-      }
-    }
-  },
-  directives: {
-    swiper: directive
-  },
-}
+// // vue2 写法
+// export default {
+//   components: {
+//   },
+//   data () {
+//     return {
+//       swiperOption: {
+//         slidesPerView: 3, // 一行显示多少张
+//         spaceBetween: 10,
+//         // 不需要配置点
+//         // pagination: {
+//         //   el: '.swiper-pagination'
+//         // },
+//         // ...
+//       },
+//       musicList: [], // 推荐列表数据
+//     }
+//   },
+//   methods: {
+//     formatValue: function (num) {
+//       let res = 0
+//       if (num >= 100000000) {
+//         res = (num / 100000000).toFixed(2) + '亿'
+//       } else if (num >= 10000) {
+//         res = (num / 10000).toFixed(2) + '万'
+//       }
+//       return res
+//     }
+//   },
+//   directives: {
+//     swiper: directive
+//   },
+//   mounted: async function () {
+//     let res = await getMusicList()
+//     this.musicList = res.data.result
+//     console.log('res', res);
+//   },
+//   // 过滤函数
+//   filter: {
+//   }
+// }
 </script>
 
 <style lang="less" scoped>
@@ -78,16 +101,16 @@ export default {
   .mlist {
     .swiper-container {
       width: 100%;
-      height: 4rem;
+      height: 3rem;
       .swiper-slide {
         text-align: center;
         font-size: 18px;
-        background: orange;
         /* Center slide text vertically */
         display: -webkit-box;
         display: -ms-flexbox;
         display: -webkit-flex;
         display: flex;
+        flex-direction: column;
         -webkit-box-pack: center;
         -ms-flex-pack: center;
         -webkit-justify-content: center;
@@ -96,6 +119,25 @@ export default {
         -ms-flex-align: center;
         -webkit-align-items: center;
         align-items: center;
+        img {
+          width: 100%;
+          height: auto;
+          border-radius: 0.1rem;
+        }
+        .name {
+          height: 0.6rem;
+          width: 100%;
+          font-size: 0.26rem;
+        }
+        .count {
+          position: absolute;
+          right: 0.2rem;
+          top: 0.15rem;
+          font-size: 0.24rem;
+          color: #ddd;
+          display: flex;
+          align-items: center;
+        }
       }
     }
   }
